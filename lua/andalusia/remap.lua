3c982bdacf5ba3c09f843aa3b86110ce2eb8756a
andalusia <3dsdreamcast@gmail.com>
2023-11-15 16:06:08 +0300

    yxa

--- lua/andalusia/remap.lua
+++ lua/andalusia/remap.lua
@@ -10,11 +10,6 @@ vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 
-vim.keymap.set("x", "<leader>p", [["_dP]])
-
-vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
-vim.keymap.set("n", "<leader>Y", [["+Y]])
-
 vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
 
 vim.keymap.set("i", "<C-c>", "<Esc>")
